39a3565b609aa91f197f744f4d01ec933302f316
NikitaMulyar <nickm.school@mail.ru>
2024-03-31 11:12:25 +0300

    Сохранение ответа в json file

--- request_data.py
+++ request_data.py
@@ -1,11 +1,12 @@
 import requests
+import json
 
 headers = {'Content-Type': 'application/json', 'X-Auth-Token': 'ppo_10_11019'}
 # 1 получаем даты
-# r = requests.get('https://olimp.miet.ru/ppo_it_final/date', headers=headers)
+# r = requests.get('https://olimp.miet.ru/ppo_it_final/date', headers=headers).json()
 
 # 2 получаем по дате
-# r = requests.get('https://olimp.miet.ru/ppo_it_final?day=25&month=01&year=23', headers=headers)
+r = requests.get('https://olimp.miet.ru/ppo_it_final?day=25&month=01&year=23', headers=headers).json()
 
 # 3 запрос проверки
 # payload = """{
@@ -20,9 +21,6 @@ headers = {'Content-Type': 'application/json', 'X-Auth-Token': 'ppo_10_11019'}
 # 	},
 # 	"date": "25-01-23"
 # }"""
-# r = requests.post('https://olimp.miet.ru/ppo_it_final?day=25&month=01&year=23', headers=headers, data=payload)
+# r = requests.post('https://olimp.miet.ru/ppo_it_final?day=25&month=01&year=23', headers=headers, data=payload).json()
 
-r.encoding = "utf-8"
-with open("file.json", "wt", encoding="utf8") as f:
-    f.write(r.text)
-print(r.text)
+json.dump(r, open('file.json', 'w'))
