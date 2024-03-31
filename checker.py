00ff29d2fed37880689ae2c8369d74fe7d3b6339
progetto65 <tmandzhiev2@gmail.com>
2024-03-31 14:23:10 +0300

    checker

--- checker.py
+++ checker.py
@@ -18,32 +18,27 @@ json_response = {"message": {"date": {"data": 1674594000,
                              "windows_for_flat": {"data": [3, 2, 1],
                                                   "description": "\u041a\u043e\u043b\u0438\u0447\u0435\u0441\u0442\u0432\u043e \u043e\u043a\u043e\u043d \u0432 \u043a\u0430\u0436\u0434\u043e\u0439 \u0438\u0437 \u043a\u043e\u043c\u043d\u0430\u0442 \u043d\u0430 \u044d\u0442\u0430\u0436\u0435 \u0441\u043b\u0435\u0432\u0430 \u043d\u0430\u043f\u0440\u0430\u0432\u043e"}}}
 
-mp = fg.rooms(json_response)
 
-dat = mp["date"].strftime("%d-%m-") + mp["date"].strftime("%Y")[2:]
-print(dat)
+def check(json_response):
+    mp = fg.rooms(json_response)
 
-headers = {'Content-Type': 'application/json', 'X-Auth-Token': 'ppo_10_11019'}
-# payload = {
-#     "data": {
-#         "count": mp["turned_on"],
-#         "rooms": mp["rooms"]
-#     },
-#     "date": dat
-# }
-payload = {
-	"data": {
-		"count": 4,
-		"rooms": [
-			3,
-			5,
-			9,
-			10
-		]
-	},
-	"date": "01-05-21"
-}
-print(payload)
-r = requests.post('https://olimp.miet.ru/ppo_it_final', headers=headers, data=payload)
-print(r)
-#json.dump(r, open('file.json', 'w'))
+    dat = mp["date"].strftime("%d-%m-") + mp["date"].strftime("%Y")[2:]
+    print(dat)
+
+    headers = {"Content-Type": "application/json", "X-Auth-Token": "ppo_10_21931"}
+    payload = {
+        "data": {
+            "count": mp["turned_on"],
+            "rooms": mp["rooms"]
+        },
+        "date": dat
+    }
+    r = requests.post('https://olimp.miet.ru/ppo_it_final', headers=headers, data=payload).json()
+    #print(r)
+    #json.dump(r, open('file.json', 'w'))
+    return r == r
+
+
+
+if __name__ == "__main__":
+    check(json_response)
