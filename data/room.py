de14bc78d72d1e1a97faa653dc2f6ce09d941193
NikitaMulyar <nickm.school@mail.ru>
2024-03-31 12:42:57 +0300

    Создание матрицы для проверки корректности комнат

--- data/room.py
+++ data/room.py
@@ -1,3 +1,5 @@
+import datetime
+
 import sqlalchemy
 from .db_session import SqlAlchemyBase
 
--- funcs_back.py
+++ funcs_back.py
@@ -2,9 +2,10 @@ import datetime
 
 from data import db_session
 from data.window import Window
+from predprof_24.data.room import Room
 
 
-def make_matrix_for_front(date: datetime.datetime):
+def make_matrix_for_windows(date: datetime.datetime):
     db_sess = db_session.create_session()
     ws = db_sess.query(Window).filter(Window.date == date).all()
     floors = {w.floor_number for w in ws}
@@ -19,4 +20,15 @@ def make_matrix_for_front(date: datetime.datetime):
         cur_fl = floors[i]
         for el in ws[cur_fl]:
             matrix[i].append((el.room_number, el.state))
+    db_sess.close()
     return matrix
+
+
+def make_matrix_for_rooms():
+    db_sess = db_session.create_session()
+    rooms = db_sess.query(Room).all()
+    rooms = sorted(rooms, key=lambda r: (r.date, r.number))
+    # Проверка корректности
+    rooms = [[r.date, r.number, True] for r in rooms]
+    db_sess.close()
+    return rooms
